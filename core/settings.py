63ba91580c6aeed2d8f7b17b2d6eb945ada34ebe
Cloudy009 <angia9510@gmail.com>
2025-05-16 16:45:51 +0700

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -248,8 +248,9 @@ STATICFILES_DIRS = (
 )
 
 if not DEBUG:
-   STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
